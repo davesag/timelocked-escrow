6719e8c8b57ad57e4505cd2ffa2fa2433b3e68b2
davesag <davesag@gmail.com>
2017-12-15 09:22:56 +0530

    n/a added tests and logic for whitelisting and unwhitelisting

--- test/creation/TimelockedEscrow_test.js
+++ test/creation/TimelockedEscrow_test.js
@@ -1,11 +1,21 @@
-const IdentityOwner = artifacts.require('./TimelockedEscrow.sol')
+const TimelockedEscrow = artifacts.require('./TimelockedEscrow.sol')
 
 contract('TimelockedEscrow', (accounts) => {
   const [superuser] = accounts
 
-  it('is owned by superuser', () => IdentityOwner.deployed()
-    .then(instance => instance.owner())
-    .then((owner) => {
-      assert.equal(owner, superuser, `Expected the owner to be '${superuser}'`)
-    }))
+  let escrow
+
+  before(async () => {
+    escrow = await TimelockedEscrow.deployed()
+  })
+
+  it('is owned by superuser', async () => {
+    const owner = await escrow.owner.call()
+    assert.equal(owner, superuser, `Expected the owner to be '${superuser}'`)
+  })
+
+  it('has timelockPeriod of 28 days', async () => {
+    const timelockPeriod = await escrow.timelockPeriod.call()
+    assert.equal(timelockPeriod.toNumber(), 28, 'Expected the timelockPeriod to be 28')
+  })
 })
