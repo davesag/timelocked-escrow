6bebf489a802f537dd6e6ad0798e035b66ae3725
davesag <davesag@gmail.com>
2017-12-18 14:38:29 +0530

    n/a updated project name, fixed some spelling, added docs

--- contracts/MockKEY.sol
+++ contracts/MockKEY.sol
@@ -3,11 +3,19 @@ pragma solidity ^0.4.18;
 import 'zeppelin-solidity/contracts/token/StandardToken.sol';
 
 
+/**
+ *  A dummy ERC20 token used for testing.
+ */
 contract MockKEY is StandardToken {
     string public constant name = 'SelfKey'; //solhint-disable-line const-name-snakecase
     string public constant symbol = 'KEY'; //solhint-disable-line const-name-snakecase
     uint256 public constant decimals = 18; //solhint-disable-line const-name-snakecase
 
+    /**
+     *  Give an address an amount of tokens.
+     *  @param punter — the address to give tokens to.
+     *  @param amount — the amount of tokens to give.
+     */
     function freeMoney(address punter, uint amount) external {
         require(punter != 0x0);
         balances[punter] = amount;
